087fb15333a8b912edb3f305edd9a8fe1173e8a8
waleedtarbosh <waleedtarbush@gmail.com>
2026-05-10 10:13:57 -0400

    feat: add workingTitle state and cancel edit functionality

--- src/features/TodoList/TodoListItem.jsx
+++ src/features/TodoList/TodoListItem.jsx
@@ -3,11 +3,30 @@ import TextInputWithLabel from '../../shared/TextInputWithLabel';
 
 function TodoListItem({ todo, onCompleteTodo }) {
   const [isEditing, setIsEditing] = useState(false);
+  const [workingTitle, setWorkingTitle] = useState(todo.title);
+
+  const handleCancel = () => {
+    setWorkingTitle(todo.title);
+    setIsEditing(false);
+  };
+
+  const handleEdit = (event) => {
+    setWorkingTitle(event.target.value);
+  };
+
   return (
     <li>
       <form>
         {isEditing ? (
-          <TextInputWithLabel value={todo.title} />
+          <>
+            <TextInputWithLabel 
+              value={workingTitle} 
+              onChange={handleEdit} 
+            />
+            <button type="button" onClick={handleCancel}>
+              Cancel
+            </button>
+          </>
         ) : (
           <>
             <label>
