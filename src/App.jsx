3f1eb21d5868e99e4ccdd02baca47f3259799a17
waleedtarbosh <waleedtarbush@gmail.com>
2026-05-19 11:20:28 -0400

    feat: update App component with auth state and conditional rendering

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,24 @@
+import "./App.css";
+import { useState } from 'react';
 import Header from './shared/Header';
 import TodosPage from './features/Todos/TodosPage';
+import Logon from './features/Logon';
 
 export default function App() {
+  const [email, setEmail] = useState('');
+  const [token, setToken] = useState('');
+
   return (
     <>
-      <Header />
-      <TodosPage />
+      <Header token={token} onSetToken={setToken} onSetEmail={setEmail} />
+      
+      <main>
+        {token ? (
+          <TodosPage token={token} />
+        ) : (
+          <Logon onSetEmail={setEmail} onSetToken={setToken} />
+        )}
+      </main>
     </>
   );
 }
