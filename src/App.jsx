e002dde40441cb905757c683ef97404b76efb316
waleedtarbosh <waleedtarbush@gmail.com>
2026-06-08 12:35:46 -0400

    feat: setup react router v7 and create HomePage

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,16 @@
 import "./App.css";
+import { Routes, Route } from "react-router";
+
 import Header from "./shared/Header";
-import TodosPage from "./features/Todos/TodosPage";
-import Logon from "./features/Logon";
-import { useAuth } from "./contexts/AuthContext";
 
 export default function App() {
-  const { isAuthenticated, email } = useAuth();
-
   return (
     <>
       <Header />
-
       <main>
-        {isAuthenticated ? (
-          <div>
-            <p
-              style={{ textAlign: "center", color: "green", margin: "10px 0" }}
-            >
-              Logged in as: {email}
-            </p>
-            <TodosPage />
-          </div>
-        ) : (
-          <Logon />
-        )}
+        <Routes>
+          
+        </Routes>
       </main>
     </>
   );
